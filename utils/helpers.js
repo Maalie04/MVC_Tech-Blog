1896a542afc0582600e8967ecf6884be3e115ec8
Jamaal Lawson <maaliemaal@gmail.com>
2021-08-31 13:23:49 -0400

    added helpers

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,15 @@
 module.exports = {
-  get_emoji: () => {
-    const randomNum = Math.random();
-    let book = "📗";
 
-    if (randomNum > 0.7) {
-      book = "📘";
-    } else if (randomNum > 0.4) {
-      book = "📙";
+    format_plural: (word, amount) => {
+      if(amount !== 1){
+        return '${word}s'
+      }
+      return '${word}'
+    },
+    
+    format_date: date => {
+      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
+          date
+      ).getFullYear()}`;
     }
-
-    return `<span for="img" aria-label="book">${book}</span>`;
-  },
-};
+  }
