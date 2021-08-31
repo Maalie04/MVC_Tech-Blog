f0ecde0cefff319c6f9cc179542534257343ffb9
Jamaal Lawson <maaliemaal@gmail.com>
2021-08-31 13:09:13 -0400

    added a function that can handle a user sign up

---
+++ public/logIn.js
@@ -0,0 +1,25 @@
+async function logInFormHandler(event){
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').value.trim();
+    const password = document.querySelector('#password-login').value.trim();
+
+    if(username && password){
+        const response = await fetch('/api/users/logIn', {
+            method: 'POSt',
+            body: JSON.stringify({
+                username,
+                password
+            }),
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+        if(response.ok){
+            document.location.replace('/');
+        }else{
+            alert(response.statusText);
+        }
+    }
+};
+
+document.querySelector('#login-form').addEventListener('submit', logOutButtonHandler);
